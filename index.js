21dce9061fdc00a7d6739359b0654040ce8ec614
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-05-31 17:33:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -66,13 +66,8 @@ const exec = (cmd, args = []) =>
  */
 
 const commitFile = async () => {
-  await exec("git", [
-    "config",
-    "--global",
-    "user.email",
-    "xxmajonezxx@gmail.com",
-  ]);
-  await exec("git", ["config", "--global", "user.name", "igorkowalczyk", "user.email", "xxmajonezxx@gmail.com"]);
+  await exec("git", ["config", "--global", "user.email", "xxmajonezxx@gmail.com",]);
+  await exec("git", ["config", "--global", "user.name", "igorkowalczyk"]);
   await exec("git", ["add", "README.md"]);
   await exec("git", [
     "commit",
