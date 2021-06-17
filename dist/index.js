60690d2f6f2c71b5279358d0c2489c5b65a88f0b
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-06-17 14:03:55 +0200

    Compile

--- dist/index.js
+++ dist/index.js
@@ -1716,9 +1716,20 @@ const serializers = {
     )} in ${toUrlFormat(item.repo.name, null, item.public)}`;
   },
   PushEvent: (item) => {
-    return `${actionIcon("commit", "📝")} Made \`${item.payload.size}\` commit${
-      item.payload.size === 1 ? "" : "s"
-    } in ${toUrlFormat(item.repo.name, null, item.public)}`;
+    if (
+      item.repo.name == "igorkowalczyk" ||
+      item.repo.name == "igorkowalczyk/igorkowalczyk"
+    ) {
+      return console.log("Testing");
+    } else {
+      return `${actionIcon("commit", "📝")} Made \`${
+        item.payload.size
+      }\` commit${item.payload.size === 1 ? "" : "s"} in ${toUrlFormat(
+        item.repo.name,
+        null,
+        item.public
+      )}`;
+    }
   },
   ReleaseEvent: (item) => {
     return `${actionIcon("release", "🏷")} Released ${
