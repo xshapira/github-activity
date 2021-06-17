318cb0192ba07166230a9aa9fe3c7fa24c3f0459
Igor Kowalczyk <xxmajonezxx@gmail.com>
2021-06-17 14:18:32 +0200

    Build

--- dist/index.js
+++ dist/index.js
@@ -1563,33 +1563,12 @@ const urlPrefix = "https://github.com/";
 
 const toUrlFormat = (item, branch, public = true) => {
  if (typeof item === "object") {
-  return Object.hasOwnProperty.call(item.payload, "issue")
-   ? public
-     ? `[\`#${item.payload.issue.number}\`](${urlPrefix}/${
-        item.repo.name
-       }/issues/${
-        item.payload.issue.number
-       } '${item.payload.issue.title.replace(/'/g, "\\'")}')`
-     : `\`#${item.payload.issue.number}\``
-   : public
-   ? `[\`#${item.payload.pull_request.number}\`](${urlPrefix}/${
-      item.repo.name
-     }/pull/${
-      item.payload.pull_request.number
-     } '${item.payload.pull_request.title.replace(/'/g, "\\'")}')`
-   : `\`#${item.payload.pull_request.number}\``;
+  return Object.hasOwnProperty.call(item.payload, "issue") ? (public ? `[\`#${item.payload.issue.number}\`](${urlPrefix}/${item.repo.name}/issues/${item.payload.issue.number} '${item.payload.issue.title.replace(/'/g, "\\'")}')` : `\`#${item.payload.issue.number}\``) : public ? `[\`#${item.payload.pull_request.number}\`](${urlPrefix}/${item.repo.name}/pull/${item.payload.pull_request.number} '${item.payload.pull_request.title.replace(/'/g, "\\'")}')` : `\`#${item.payload.pull_request.number}\``;
  }
- return !public
-  ? branch
-    ? `\`${branch}\``
-    : `<span title="Private Repo">\`🔒${item}\`</span>`
-  : `[${branch ? `\`${branch}\`` : item}](${urlPrefix}${item}${
-     branch ? `/tree/${branch}` : ""
-    })`;
+ return !public ? (branch ? `\`${branch}\`` : `<span title="Private Repo">\`🔒${item}\`</span>`) : `[${branch ? `\`${branch}\`` : item}](${urlPrefix}${item}${branch ? `/tree/${branch}` : ""})`;
 };
 
-const actionIcon = (name, alt) =>
- `<a href="https://github.com/igorkowalczyk" title="${alt}"><img alt="${alt}" src="https://github.com/igorkowalczykbot/github-activity/raw/master/icons/${name}.png" align="top" height="18"></a>`;
+const actionIcon = (name, alt) => `<a href="https://github.com/igorkowalczyk" title="${alt}"><img alt="${alt}" src="https://github.com/igorkowalczykbot/github-activity/raw/master/icons/${name}.png" align="top" height="18"></a>`;
 
 /**
  * Execute shell command
@@ -1624,130 +1603,55 @@ const exec = (cmd, args = []) =>
  */
 
 const commitFile = async () => {
- await exec("git", [
-  "config",
-  "--global",
-  "user.email",
-  "igor.kowalczyk.bot@onet.pl",
- ]);
+ await exec("git", ["config", "--global", "user.email", "igor.kowalczyk.bot@onet.pl"]);
  await exec("git", ["config", "--global", "user.name", "IgorKowalczykBot"]);
  await exec("git", ["add", "README.md"]);
- await exec("git", [
-  "commit",
-  "-m",
-  "[Bot] Update README with the recent activity",
- ]);
+ await exec("git", ["commit", "-m", "[Bot] Update README with the recent activity"]);
  await exec("git", ["push"]);
 };
 
 const serializers = {
  CommitCommentEvent: (item) => {
   const hash = item.payload.comment.commit_id.slice(0, 7);
-  return `${actionIcon("comment", "🗣")} Commented on ${
-   item.public
-    ? `[\`${hash}\`](${item.payload.comment.html_url})`
-    : `\`${hash}\``
-  } in ${toUrlFormat(item.repo.name, null, item.public)}`;
+  return `${actionIcon("comment", "🗣")} Commented on ${item.public ? `[\`${hash}\`](${item.payload.comment.html_url})` : `\`${hash}\``} in ${toUrlFormat(item.repo.name, null, item.public)}`;
  },
  CreateEvent: (item) => {
-  if (item.payload.ref_type === "repository")
-   return `${actionIcon("create-repo", "➕")} Created repository ${toUrlFormat(
-    item.repo.name,
-    null,
-    item.public
-   )}`;
-  if (item.payload.ref_type === "branch")
-   return `${actionIcon("create-branch", "📂")} Created branch ${toUrlFormat(
-    item.repo.name,
-    item.payload.ref,
-    item.public
-   )} in ${toUrlFormat(item.repo.name, null, item.public)}`;
+  if (item.payload.ref_type === "repository") return `${actionIcon("create-repo", "➕")} Created repository ${toUrlFormat(item.repo.name, null, item.public)}`;
+  if (item.payload.ref_type === "branch") return `${actionIcon("create-branch", "📂")} Created branch ${toUrlFormat(item.repo.name, item.payload.ref, item.public)} in ${toUrlFormat(item.repo.name, null, item.public)}`;
  },
  DeleteEvent: (item) => {
-  return `${actionIcon("delete", "❌")} Deleted \`${
-   item.payload.ref
-  }\` from ${toUrlFormat(item.repo.name, null, item.public)}`;
+  return `${actionIcon("delete", "❌")} Deleted \`${item.payload.ref}\` from ${toUrlFormat(item.repo.name, null, item.public)}`;
  },
  ForkEvent: (item) => {
-  return `${actionIcon("fork", "🍴")} Forked ${toUrlFormat(
-   item.repo.name,
-   null,
-   item.public
-  )} to ${toUrlFormat(
-   item.payload.forkee.full_name,
-   null,
-   item.payload.forkee.public
-  )}`;
+  return `${actionIcon("fork", "🍴")} Forked ${toUrlFormat(item.repo.name, null, item.public)} to ${toUrlFormat(item.payload.forkee.full_name, null, item.payload.forkee.public)}`;
  },
  IssueCommentEvent: (item) => {
-  return `${actionIcon("comment", "🗣")} Commented on ${toUrlFormat(
-   item,
-   null,
-   item.public
-  )} in ${toUrlFormat(item.repo.name, null, item.public)}`;
+  return `${actionIcon("comment", "🗣")} Commented on ${toUrlFormat(item, null, item.public)} in ${toUrlFormat(item.repo.name, null, item.public)}`;
  },
  IssuesEvent: (item) => {
-  return `${actionIcon("issue", "❗️")} ${capitalize(
-   item.payload.action
-  )} issue ${toUrlFormat(item, null, item.public)} in ${toUrlFormat(
-   item.repo.name,
-   null,
-   item.public
-  )}`;
+  return `${actionIcon("issue", "❗️")} ${capitalize(item.payload.action)} issue ${toUrlFormat(item, null, item.public)} in ${toUrlFormat(item.repo.name, null, item.public)}`;
  },
  PullRequestEvent: (item) => {
-  const emoji =
-   item.payload.action === "opened"
-    ? actionIcon("pr-open", "✅")
-    : actionIcon("pr-close", "❌");
-  const line = item.payload.pull_request.merged
-   ? `${actionIcon("merge", "🎉")} Merged`
-   : `${emoji} ${capitalize(item.payload.action)}`;
-  return `${line} PR ${toUrlFormat(item, null, item.public)} in ${toUrlFormat(
-   item.repo.name,
-   null,
-   item.public
-  )}`;
+  const emoji = item.payload.action === "opened" ? actionIcon("pr-open", "✅") : actionIcon("pr-close", "❌");
+  const line = item.payload.pull_request.merged ? `${actionIcon("merge", "🎉")} Merged` : `${emoji} ${capitalize(item.payload.action)}`;
+  return `${line} PR ${toUrlFormat(item, null, item.public)} in ${toUrlFormat(item.repo.name, null, item.public)}`;
  },
  PullRequestReviewEvent: (item) => {
-  return `${actionIcon("review", "🔍")} Reviewed ${toUrlFormat(
-   item,
-   null,
-   item.public
-  )} in ${toUrlFormat(item.repo.name, null, item.public)}`;
+  return `${actionIcon("review", "🔍")} Reviewed ${toUrlFormat(item, null, item.public)} in ${toUrlFormat(item.repo.name, null, item.public)}`;
  },
  PushEvent: (item) => {
   tools.log.debug(item.repo.name);
-  return `${actionIcon("commit", "📝")} Made \`${item.payload.size}\` commit${
-   item.payload.size === 1 ? "" : "s"
-  } in ${toUrlFormat(item.repo.name, null, item.public)}`;
+  return `${actionIcon("commit", "📝")} Made \`${item.payload.size}\` commit${item.payload.size === 1 ? "" : "s"} in ${toUrlFormat(item.repo.name, null, item.public)}`;
  },
  ReleaseEvent: (item) => {
-  return `${actionIcon("release", "🏷")} Released ${
-   item.public
-    ? `[\`${item.payload.release.tag_name}\`](${item.payload.release.html_url})`
-    : `\`${item.payload.release.tag_name}\``
-  } in ${toUrlFormat(item.repo.name, null, item.public)}`;
+  return `${actionIcon("release", "🏷")} Released ${item.public ? `[\`${item.payload.release.tag_name}\`](${item.payload.release.html_url})` : `\`${item.payload.release.tag_name}\``} in ${toUrlFormat(item.repo.name, null, item.public)}`;
  },
  WatchEvent: (item) => {
-  return `${actionIcon("star", "⭐")} Starred ${toUrlFormat(
-   item.repo.name,
-   null,
-   item.public
-  )}`;
+  return `${actionIcon("star", "⭐")} Starred ${toUrlFormat(item.repo.name, null, item.public)}`;
  },
 };
 
-const timestamper = (item) =>
- `\`[${item.created_at
-  .split("T")[0]
-  .split("-")
-  .slice(1, 3)
-  .join("/")} ${item.created_at
-  .split("T")[1]
-  .split(":")
-  .slice(0, 2)
-  .join(":")}]\``;
+const timestamper = (item) => `\`[${item.created_at.split("T")[0].split("-").slice(1, 3).join("/")} ${item.created_at.split("T")[1].split(":").slice(0, 2).join(":")}]\``;
 
 Toolkit.run(
  async (tools) => {
@@ -1775,12 +1679,7 @@ Toolkit.run(
   // 	page: 2
   // });
 
-  tools.log.debug(
-   `Activity for ${GH_USERNAME}, ${eventArrs.reduce(
-    (a, c) => a + c.data.length,
-    0
-   )} events found.`
-  );
+  tools.log.debug(`Activity for ${GH_USERNAME}, ${eventArrs.reduce((a, c) => a + c.data.length, 0)} events found.`);
 
   const last = (array) => array[array.length - 1];
 
@@ -1788,13 +1687,7 @@ Toolkit.run(
 
   for (const events of eventArrs) {
    for (const data of events.data) {
-    if (
-     arr.length &&
-     data.type === "PushEvent" &&
-     last(arr).type === "PushEvent" &&
-     data.repo.name === last(arr).repo.name
-    )
-     arr[arr.length - 1].payload.size += data.payload.size;
+    if (arr.length && data.type === "PushEvent" && last(arr).type === "PushEvent" && data.repo.name === last(arr).repo.name) arr[arr.length - 1].payload.size += data.payload.size;
     else arr.push(data);
    }
   }
@@ -1811,28 +1704,20 @@ Toolkit.run(
    // Call the serializer to construct a string
    .map((item) => `${timestamper(item)} ${serializers[item.type](item)}`)
    // Filter out undefined lines
-   .filter(
-    (item) => !item.match(/^`\[\d{1,2}\/\d{1,2} \d{1,2}:\d{2}]` undefined$/)
-   );
+   .filter((item) => !item.match(/^`\[\d{1,2}\/\d{1,2} \d{1,2}:\d{2}]` undefined$/));
 
   const readmeContent = fs.readFileSync("./README.md", "utf-8").split("\n");
 
   // Find the index corresponding to <!--START_SECTION:activity--> comment
-  let startIdx = readmeContent.findIndex(
-   (content) => content.trim() === "<!--START_SECTION:activity-->"
-  );
+  let startIdx = readmeContent.findIndex((content) => content.trim() === "<!--START_SECTION:activity-->");
 
   // Early return in case the <!--START_SECTION:activity--> comment was not found
   if (startIdx === -1) {
-   return tools.exit.failure(
-    `Couldn't find the <!--START_SECTION:activity--> comment. Exiting!`
-   );
+   return tools.exit.failure(`Couldn't find the <!--START_SECTION:activity--> comment. Exiting!`);
   }
 
   // Find the index corresponding to <!--END_SECTION:activity--> comment
-  const endIdx = readmeContent.findIndex(
-   (content) => content.trim() === "<!--END_SECTION:activity-->"
-  );
+  const endIdx = readmeContent.findIndex((content) => content.trim() === "<!--END_SECTION:activity-->");
 
   if (!content.length) {
    tools.exit.failure("No events found");
@@ -1847,21 +1732,7 @@ Toolkit.run(
   if (startIdx !== -1) {
    // Add one since the content needs to be inserted just after the initial comment
    startIdx++;
-   content.forEach((line, idx) =>
-    readmeContent.splice(
-     startIdx + idx,
-     0,
-     `${
-      idx === 10
-       ? "\n<details><summary>⚡ Show more activity</summary>\n\n"
-       : ""
-     }${line}  ${
-      idx === content.length - 1
-       ? "\n\n</details>\n<!--END_SECTION:activity-->"
-       : ""
-     }`
-    )
-   );
+   content.forEach((line, idx) => readmeContent.splice(startIdx + idx, 0, `${idx === 10 ? "\n<details><summary>⚡ Show more activity</summary>\n\n" : ""}${line}  ${idx === content.length - 1 ? "\n\n</details>\n<!--END_SECTION:activity-->" : ""}`));
 
    // // Append <!--END_SECTION:activity--> comment
    // readmeContent.splice(
